6c61c28848c7200fa27e43e18de421b5a095ab9d
alicjac0 <157830089+alicjac0@users.noreply.github.com>
2026-01-11 21:47:57 +0100

    Add Vector2D class for 2D physics calculations

---
+++ src/physics/Vector2D.java
@@ -0,0 +1,27 @@
+package physics;
+
+public class Vector2D {
+    public double x;
+    public double y;
+
+    public Vector2D(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public Vector2D add(Vector2D v){
+        return new Vector2D(this.x+ v.x, this.y+v.y);
+    }
+
+    public Vector2D subtract(Vector2D v){
+        return new Vector2D(this.x- v.x, this.y-v.y);
+    }
+
+    public Vector2D multiply(double scalar){
+        return new Vector2D(this.x*scalar, this.y*scalar);
+    }
+
+    public double length(){
+        return Math.sqrt(x*x + y*y);
+    }
+}
