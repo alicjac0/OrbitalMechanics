7ab708e7dd991bb3153eb3d1f6c860e7612a9228
alicjac0 <157830089+alicjac0@users.noreply.github.com>
2026-01-11 22:13:48 +0100

    Add Body class representing physical objects

--- src/simulation/Body.java
+++ src/simulation/Body.java
@@ -1,4 +1,15 @@
 package simulation;
 
+import physics.Vector2D;
+
 public class Body {
+    public double mass;
+    public Vector2D position;
+    public Vector2D velocity;
+
+    public Body(double mass, Vector2D position, Vector2D velocity){
+        this.mass=mass;
+        this.position=position;
+        this.velocity= velocity;
+    }
 }
